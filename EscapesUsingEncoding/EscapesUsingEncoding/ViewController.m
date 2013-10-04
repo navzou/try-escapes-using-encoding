84ef0945b3d08af80b7ec6e0cf8500889ffea8c3
jerzyz <251654549@qq.com>
2013-10-04 23:25:25 +0800

    try-escapse-using-encoding

---
+++ EscapesUsingEncoding/EscapesUsingEncoding/ViewController.m
@@ -0,0 +1,41 @@
+//
+//  ViewController.m
+//  EscapesUsingEncoding
+//
+//  Created by navzou on 10/4/13.
+//  Copyright (c) 2013 navzou. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+    [self main];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (void)main
+{
+    NSString *str = @"The Objective-C language";
+    
+    str = [str stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
+    NSLog(@"%@", str);
+    
+    str = [str stringByReplacingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
+    NSLog(@"%@", str);
+}
+
+@end
